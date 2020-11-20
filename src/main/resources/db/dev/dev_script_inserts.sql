6cdcbc0e6e20b9fbb7fdb520d1c8dec5c4942e19
matheusCalaca <matheusfcalaca@gmail.com>
2020-11-20 20:20:38 -0300

    change coluns names in sql

--- src/main/resources/db/dev/dev_script_inserts.sql
+++ src/main/resources/db/dev/dev_script_inserts.sql
@@ -1,10 +1,10 @@
-INSERT INTO user.user (id, cpf, dataNascimento, email, nome, senha, sobrenome)
+INSERT INTO user.user (id, cpf, dateOfBirth, email, name, password, lastname)
 VALUES (1, '12345678909', null, 'teste@gmail.com', 'matheus ', 'sq/7bDExYjl5OB06IjU1ix8G5U6I+YAdWrw+zDn7a7w=', 'calaça');
 
-INSERT INTO user.user (id, cpf, dataNascimento, email, nome, senha, sobrenome)
+INSERT INTO user.user (id, cpf, dateOfBirth, email, name, password, lastname)
 VALUES (2, '12345678908', null, 'teste2@gmail.com', 'admin', 'sq/7bDExYjl5OB06IjU1ix8G5U6I+YAdWrw+zDn7a7w=', '');
 
-INSERT INTO user.user (id, cpf, dataNascimento, email, nome, senha, sobrenome)
+INSERT INTO user.user (id, cpf, dateOfBirth, email, name, password, lastname)
 VALUES (3, '12345678907', null, 'teste3@gmail.com', 'user', 'sq/7bDExYjl5OB06IjU1ix8G5U6I+YAdWrw+zDn7a7w=', '');
 
 
