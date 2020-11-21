cec747c2b2a29aed8ada6eb7ca6e64c2977ce959
matheusCalaca <matheusfcalaca@gmail.com>
2020-11-20 23:13:35 -0300

    correção palavra reservada

--- src/main/resources/db/dev/dev_script_inserts.sql
+++ src/main/resources/db/dev/dev_script_inserts.sql
@@ -8,6 +8,6 @@ INSERT INTO USUARIO (id, cpf, dateOfBirth, email, name, password, lastname)
 VALUES (3, '12345678907', null, 'teste3@gmail.com', 'user', 'sq/7bDExYjl5OB06IjU1ix8G5U6I+YAdWrw+zDn7a7w=', '');
 
 
-INSERT INTO user_roles (USER_id, roles) VALUES (1, 'USER');
-INSERT INTO user_roles (USER_id, roles) VALUES (2, 'ADMIN');
-INSERT INTO user_roles (USER_id, roles) VALUES (2, 'USER');
+INSERT INTO user_roles (USUARIO_id, roles) VALUES (1, 'USER');
+INSERT INTO user_roles (USUARIO_id, roles) VALUES (2, 'ADMIN');
+INSERT INTO user_roles (USUARIO_id, roles) VALUES (2, 'USER');
