d9b4b9f3919cd0d41d3a0854402d69426a2395da
matheusCalaca <matheusfcalaca@gmail.com>
2020-12-28 22:33:05 -0300

    removendo kafka aplication

--- src/main/java/br/com/matheusCalaca/user/kafka/KafkaRest.java
+++ src/main/java/br/com/matheusCalaca/user/kafka/KafkaRest.java
@@ -1,41 +1,41 @@
-package br.com.matheusCalaca.user.kafka;
-
-import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-
-import br.com.matheusCalaca.user.model.User;
-import org.eclipse.microprofile.reactive.messaging.Channel;
-import org.eclipse.microprofile.reactive.messaging.Emitter;
-import org.jboss.resteasy.annotations.SseElementType;
-import org.reactivestreams.Publisher;
-
-@Path("/kafka")
-public class KafkaRest {
-
-
-    @Inject
-    @Channel("user-stream")
-    Publisher<User> mensagemConsumer;
-
-    @Inject
-    @Channel("user-stream")
-    Emitter<User> mensagemEmitter;
-
-//    @POST
-//    public void insertMensagemKafka(User user) {
-//        mensagemEmitter.send(user);
-//    }
+//package br.com.matheusCalaca.user.kafka;
+//
+//import javax.inject.Inject;
+//import javax.ws.rs.GET;
+//import javax.ws.rs.POST;
+//import javax.ws.rs.Path;
+//import javax.ws.rs.Produces;
+//import javax.ws.rs.core.MediaType;
+//
+//import br.com.matheusCalaca.user.model.User;
+//import org.eclipse.microprofile.reactive.messaging.Channel;
+//import org.eclipse.microprofile.reactive.messaging.Emitter;
+//import org.jboss.resteasy.annotations.SseElementType;
+//import org.reactivestreams.Publisher;
+//
+//@Path("/kafka")
+//public class KafkaRest {
 //
 //
-//    @GET
-//    @Path("/stream")
-//    @Produces(MediaType.SERVER_SENT_EVENTS)
-//    @SseElementType("text/plain")
-//    public Publisher<User> stream() {
-//        return mensagemConsumer;
-//    }
-}
+//    @Inject
+//    @Channel("user-stream")
+//    Publisher<User> mensagemConsumer;
+//
+//    @Inject
+//    @Channel("user-stream")
+//    Emitter<User> mensagemEmitter;
+//
+////    @POST
+////    public void insertMensagemKafka(User user) {
+////        mensagemEmitter.send(user);
+////    }
+////
+////
+////    @GET
+////    @Path("/stream")
+////    @Produces(MediaType.SERVER_SENT_EVENTS)
+////    @SseElementType("text/plain")
+////    public Publisher<User> stream() {
+////        return mensagemConsumer;
+////    }
+//}
--- src/main/java/br/com/matheusCalaca/user/kafka/converter/UserDeserializer.java
+++ src/main/java/br/com/matheusCalaca/user/kafka/converter/UserDeserializer.java
@@ -1,11 +1,11 @@
-package br.com.matheusCalaca.user.kafka.converter;
-
-import br.com.matheusCalaca.user.model.User;
-import io.quarkus.kafka.client.serialization.JsonbDeserializer;
-
-public class UserDeserializer extends JsonbDeserializer<User> {
-
-    public UserDeserializer() {
-        super(User.class);
-    }
-}
+//package br.com.matheusCalaca.user.kafka.converter;
+//
+//import br.com.matheusCalaca.user.model.User;
+//import io.quarkus.kafka.client.serialization.JsonbDeserializer;
+//
+//public class UserDeserializer extends JsonbDeserializer<User> {
+//
+//    public UserDeserializer() {
+//        super(User.class);
+//    }
+//}
